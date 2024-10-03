cbdf01bc1cf436d720246d804d322a196ef35317
Lorae <stojanovic.lorae@gmail.com>
2024-10-03 10:56:15 -0400

    feat: import IPUMS data and save to database

---
+++ src/import-ipums.R
@@ -0,0 +1,27 @@
+# import-ipums.R
+#
+# This script processes raw IPUMS data and saves it in a DuckDB file.
+# 
+# Current data: usa_00005 (2022). This file is .gitignored.
+# TODO: replace manual data pull with an API call for reproducibility.
+# TODO: There's a nearly identical version of  this script in the sister
+# GitHub repo, lorae/household-size. Functionalize in `duckdata` and call 
+# in both?
+#
+# For more on IPUMS and ipumsr: https://www.youtube.com/watch?v=OT6upQ1dBgU
+
+# ----- Step 0: Load packages ----- #
+library("dplyr")
+library("duckdb")
+library("ipumsr")
+
+# ----- Step 1: Load and process IPUMS data ----- #
+
+ddi <- read_ipums_ddi("data/ipums-microdata/usa_00005.xml")
+ipums_tb <- read_ipums_micro(ddi, var_attrs = c()) 
+
+# ----- Step 2: Save to DuckDB ----- #
+
+con <- dbConnect(duckdb::duckdb(), "data/db/ipums-raw.duckdb")
+dbWriteTable(con, "ipums", ipums_tb, overwrite = TRUE)
+DBI::dbDisconnect(con)
