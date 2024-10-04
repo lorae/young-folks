c69911de60b97068a2d78d18f0c716d20fa65b7c
Lorae <stojanovic.lorae@gmail.com>
2024-10-04 13:25:35 -0400

    refactor: use create_sql_query function

--- src/process-synthetic-data.R
+++ src/process-synthetic-data.R
@@ -67,33 +67,7 @@ test_that("AGE bucket column is created correctly with extra columns", {
     AGE_bucketed = c("Under 16", "16-17", "18-22", "18-22", "23-29", "30+")  # Bucketed AGE column
   )
   
-  # Run the SQL query to create the "AGE_bucketed" column
-  query <- "
-    WITH age_buckets AS (
-        -- Defining the bucket ranges
-        SELECT 'Under 16' AS bucket_name, 0 AS lower_bound, 16 AS upper_bound
-        UNION ALL
-        SELECT '16-17', 16, 18
-        UNION ALL
-        SELECT '18-22', 18, 23
-        UNION ALL
-        SELECT '23-29', 23, 30
-        UNION ALL
-        SELECT '30+', 30, NULL  -- NULL for upper bound as infinity
-    )
-    
-    -- Applying the lookup table to the AGE column
-    SELECT 
-        data.*, 
-        COALESCE(bucket_name, 'Unknown') AS AGE_bucketed
-    FROM 
-        ipums_bucketed AS data
-    LEFT JOIN 
-        age_buckets
-    ON 
-        data.AGE >= age_buckets.lower_bound 
-        AND (data.AGE < age_buckets.upper_bound OR age_buckets.upper_bound IS NULL);
-    "
+  query <- create_sql_query()
   
   # Execute the query
   result <- dbGetQuery(con, query)
