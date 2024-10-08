206c6667c510059efc2a6a25dd1eab66362ab7aa
Lorae <stojanovic.lorae@gmail.com>
2024-10-08 15:18:10 -0400

    feat: improve accuracy of `cohabit_bin` variable using documentation in `docs/how-we-classify-families.html`

--- src/add-relational-variables.R
+++ src/add-relational-variables.R
@@ -23,10 +23,9 @@ ipums_db <- tbl(con, "ipums_processed")
 hh_counts <- ipums_db %>%
   group_by(hh_id) %>%
   summarize(
-    count_RELATE_1 = sum(if_else(RELATE == 1, 1L, 0L)),
-    count_RELATE_2 = sum(if_else(RELATE == 2, 1L, 0L)),
-    count_RELATE_5 = sum(if_else(RELATE == 5, 1L, 0L)),
-    count_RELATE_6 = sum(if_else(RELATE == 6, 1L, 0L))
+    count_RELATED_301 = sum(if_else(RELATED == 301, 1L, 0L)),
+    count_RELATED_501 = sum(if_else(RELATED == 501, 1L, 0L)),
+    count_RELATED_601 = sum(if_else(RELATE == 601, 1L, 0L))
   )
 
 # ----- Step 4: Join the counts back to the main table ----- #
@@ -34,30 +33,42 @@ hh_counts <- ipums_db %>%
 ipums_with_counts <- ipums_db %>%
   left_join(hh_counts, by = "hh_id")
 
-# ----- Step 5: Compute cohabit_parent and cohabit_parent_inlaw ----- #
+# ----- Step 5: Compute cohabit_bin ----- #
+# For documentation on how these rules were formulated and their caveats,
+# please see `docs/how-we-classify-families.html`
 
-ipums_final <- ipums_with_counts %>%
+ipums_final <- ipums_with_counts |>
   mutate(
-    cohabit_parent = case_when(
-      RELATE == 1 ~ count_RELATE_5,
-      RELATE == 2 ~ count_RELATE_6,
-      RELATE == 3 ~ count_RELATE_1 + count_RELATE_2,
-      TRUE ~ NA_real_
-    ),
-    cohabit_parent_inlaw = case_when(
-      RELATE == 1 ~ count_RELATE_6,
-      RELATE == 2 ~ count_RELATE_5,
-      RELATE == 3 ~ NA_real_,
-      TRUE ~ NA_real_
-    )
-  ) %>%
-  select( # Remove helper columns
-    -count_RELATE_1, 
-    -count_RELATE_2, 
-    -count_RELATE_5, 
-    -count_RELATE_6
-    )
-
+    # cohabit_bin equals 0 when person is not living with parent,
+    # 1 when person is living with parent, and 2 when person is living
+    # in a group setting, like a prison.
+        cohabit_bin = case_when(
+          RELATED == 101 ~ if_else(count_RELATED_501 > 0, 1, 0),
+          RELATED == 201 ~ if_else(count_RELATED_601 > 0, 1, 0),
+          RELATED == 301 ~ 1,
+          RELATED == 302 ~ 1,
+          RELATED == 303 ~ 1,
+          RELATED == 401 ~ 0,
+          RELATED == 501 ~ 0,
+          RELATED == 701 ~ if_else(count_RELATED_501 > 0, 1, 0),
+          RELATED == 801 ~ if_else(count_RELATED_601 > 0, 1, 0),
+          RELATED == 901 ~ if_else(count_RELATED_301 > 0, 1, 0),
+          RELATED == 1001 ~ 0,
+          RELATED == 1114 ~ if_else(count_RELATED_601 > 0, 1, 0),
+          RELATED == 1115 ~ 0,
+          RELATED == 1241 ~ 0,
+          RELATED == 1242 ~ 1,
+          RELATED == 1260 ~ 0,
+          RELATED == 1270 ~ 0,
+          RELATED == 1301 ~ 2,  # Special case for 1301
+          TRUE ~ NA_real_  # NA for all other cases
+        )
+      ) |>
+      select( # Remove helper columns
+        -count_RELATED_301,
+        -count_RELATED_501,
+        -count_RELATED_601
+      )
 # ----- Step 6: Save the computed result back to the db ----- # 
 
 compute(
